8e186ab09e53d579a0b8b2345e345da99ed37edb
Harshit Raj <raj.0489@gmail.com>
2018-04-01 01:29:44 -0400

    initial commit

---
+++ GeneticAlgorithm/src/main/java/neu/info6205/GeneticAlgorithm/App.java
@@ -0,0 +1,13 @@
+package neu.info6205.GeneticAlgorithm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
